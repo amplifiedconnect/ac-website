c6bb5018f354b0767a6a987fad1615cbba6285a2
Andrew <2590960+one2many@users.noreply.github.com>
2019-12-18 15:52:17 -0800

    update logo header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import aclogo from '../assets/images/Amplified_Connect-logo-wht.png'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Amplified Connect</strong> <span>by Amplified Digital</span></Link>
+        <Link to="/" className="logo"><img src={aclogo} alt="Amplified Connect" /><span>by Amplified Digital</span></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
