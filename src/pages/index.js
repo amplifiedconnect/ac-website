d78759836c5208e754a264f19a35e103f1421cb8
Andrew <2590960+one2many@users.noreply.github.com>
2019-12-19 13:11:38 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
+import sparkler from '../assets/images/sparkler.png'
+import event from '../assets/images/tucsonbestofevent.jpg'
 
 
 class HomeIndex extends React.Component {
@@ -28,31 +30,28 @@ class HomeIndex extends React.Component {
 
                 <div id="main">
                     <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${pic01})`}}>
+                        <article style={{backgroundImage: `url(${sparkler})`}}>
                             <header className="major">
                                 <h3>Celebrating local businesses</h3>
-                                <p>Connecting communities</p>
+                                <p>Our vision for connecting communities</p>
                             </header>
                             <Link to="/connect" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
+                        <article style={{backgroundImage: `url(${event})`}}>
                             <header className="major">
                                 <h3>Services</h3>
-                                <p>We are a team of media professionals.</p>
                             </header>
                             <Link to="/services" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic03})`}}>
+                        <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
                                 <h3>About us</h3>
-                                <p>We are a team of media professionals.</p>
                             </header>
                             <Link to="/about" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${pic04})`}}>
+                        <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
                                 <h3>Client Resources</h3>
-                                <p>Stuff for our clients</p>
                             </header>
                             <Link to="/clientresources" className="link primary"></Link>
                         </article>
@@ -62,7 +61,7 @@ class HomeIndex extends React.Component {
                             <header className="major">
                                 <h2>About Amplified Digital</h2>
                             </header>
-                            <p>Amplified Digital is.. tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                            <p>Amplified Digital isis a different kind of digital advertising agency. Formed out of a passion for helping companies develop a strong digital presence, over the years we’ve evolved to provide strategic digital marketing, media planning, consulting and analytics. Our goal is to help your company successfully navigate through an ever-changing digital landscape.</p>
                             <ul className="actions">
                                 <li><a href ="https://www.amplifieddigitalagency.com" className="button next">Learn about Amplified Agency</a></li>
                             </ul>
