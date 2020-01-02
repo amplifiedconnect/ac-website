580958ced59a183286f08216d406fd436f904db9
Andrew <Ahmcfadden@gmail.com>
2020-01-02 13:25:22 -0800

    Update connect.js

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import tower from '../assets/images/tower.JPG'
+import tower from '../assets/images/tower.jpg'
 
 const Generic = (props) => (
     <Layout>
@@ -17,12 +17,11 @@ const Generic = (props) => (
                     <header className="major">
                         <h1>Amplifying the Local Connnection</h1>
                     </header>
-                    <p>As the local business landsape changes, agencies must adapt products and services to the new competitive environment.</p>
                     <span className="image main"><img src={tower} alt="" /></span>
-                    <p>Amplified Connect believes...</p>
-                    <p>We offer...</p>
+                    <p>As the local business landsape changes, agencies must adapt products and services to the new competitive environment.</p>
+                    <p>Amplified Connect believes:</p>
+                    <p>We offer:</p>
                     <p>Connect with us to learn more</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
                 </div>
             </section>
         </div>
