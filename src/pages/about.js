d569f1e96b44c28c8effd1e6b04888300bda27cd
Andrew <2590960+one2many@users.noreply.github.com>
2020-01-07 19:14:47 -0800

    updated emily (again)

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet';
 import Layout from '../components/layout';
 import BannerAbout from '../components/BannerAbout';
 
-import pic08 from '../assets/images/emily2n.jpg';
+import pic08 from '../assets/images/emilytintera.png';
 import pic09 from '../assets/images/pic09.jpg';
 import pic10 from '../assets/images/Ramirez_Picture.jpg';
 import kerrbw from '../assets/images/kerrbw.jpg';
